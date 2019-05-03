8779d437044abd59d820e0a48b8cf8a58d469c30
Rafael Kahn <rafaelmk@al.insper.edu.br>
2019-05-03 09:11:17 -0300

    Adicionadas imagens do jogo e imports do game

--- SuperFox.py
+++ SuperFox.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  3 08:07:56 2019
-
-@author: lobda
-"""
-
+#Bibliotecas e importações
+import pygame
+import time
+from os import path
+import random
+#Caminho das imagens
