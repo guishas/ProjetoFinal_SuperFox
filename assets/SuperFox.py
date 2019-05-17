f86d48bd4aec9da25bcb871a593fd343e21c6033
decorocco <andrebr@al.insper.edu.br>
2019-05-17 08:56:34 -0300

    atualizada animação de andar

--- assets/SuperFox.py
+++ assets/SuperFox.py
@@ -182,7 +182,7 @@ def load_assets(img_dir, snd_dir):
     assets['background'] = pygame.image.load(path.join(img_dir, 'bg_fase1.png')).convert()
     fox_walk = []
     for i in range(1, 3, 1):
-        filename = 'fox_walk{}.png'.format(i)
+        filename = 'fox_walk1.png'.format(i)
         walk = pygame.image.load(path.join(img_dir, filename)).convert()
         walk = pygame.transform.scale(walk, (70, 58))
         walk.set_colorkey(BLACK)
