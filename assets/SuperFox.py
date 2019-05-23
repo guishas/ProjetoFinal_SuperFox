43731349163da85dcba5ed1eee8f0b4556100363
Rafael Kahn <rafaelmk@al.insper.edu.br>
2019-05-23 16:18:26 -0300

    Iniciando sistema de vida

--- assets/SuperFox.py
+++ assets/SuperFox.py
@@ -490,6 +490,7 @@ try:
     running = True
     
     score = 0
+    lifes = 3
     
     while running:
         
@@ -544,11 +545,17 @@ try:
         
         hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
         if hits:
-            death_sound.play()
-            music_sound.stop()
             player.kill()
-            time.sleep(3)
-            pygame.quit()
+            lifes -= 1
+            if lifes == 0:   
+                death_sound.play()
+                music_sound.stop()
+                player.kill()
+                time.sleep(3)
+                pygame.quit()
+            else: 
+                player = Player(assets['player_img'], assets['fox_walk'], assets['fox_jump'])
+                all_sprites.add(player)
         
         hits = pygame.sprite.groupcollide(mobs, fireballs, True, True)
         for hit in hits:
@@ -577,6 +584,8 @@ try:
         text_rect = text_surface.get_rect()
         text_rect.midtop = (WIDTH/2, 10)
         screen.blit(text_surface, text_rect)
+        
+        #colocando a vida na tela
         
         #Depois de desenhar tudo inverte o display
         pygame.display.flip()
